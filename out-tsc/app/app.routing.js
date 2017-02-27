43fb361696168bb9b4793f3fd59b9106a7ace96e
Devon Kupiec <Devonkupiec@gmail.com>
2017-02-26 19:55:20 -0600

    Auto-generated commit

--- out-tsc/app/app.routing.js
+++
@@ -1,9 +0,0 @@
-import { RouterModule } from "@angular/router";
-import { HomeComponent } from "./home.component";
-var APP_ROUTES = [
-    { path: '', component: HomeComponent },
-    { path: 'recipes', loadChildren: 'app/recipes/recipes.module#RecipesModule' },
-    { path: 'shopping-list', loadChildren: 'app/shopping-list/shopping-list.module#ShoppingListModule' }
-];
-export var routing = RouterModule.forRoot(APP_ROUTES);
-//# sourceMappingURL=D:/Dropbox/Programming/Angular/recipe-book/src/app/app.routing.js.map
--- out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=D:/Dropbox/Programming/Angular/recipe-book/src/environments/environment.prod.js.map
